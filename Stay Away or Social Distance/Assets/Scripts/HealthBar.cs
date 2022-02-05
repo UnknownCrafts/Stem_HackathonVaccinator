f1163f471f20c0c9e7686d8bbb45549304895713
S V <sv@Ss-MacBook-Pro.local>
2022-02-05 17:51:43 -0500

    Gradient for healthbar

--- Stay Away or Social Distance/Assets/Scripts/HealthBar.cs
+++ Stay Away or Social Distance/Assets/Scripts/HealthBar.cs
@@ -8,17 +8,26 @@ public class HealthBar : MonoBehaviour
 
     Slider slider;
 
+    public Gradient gradient;
+
+    Image fill;
+
     void Start() {
         slider = gameObject.GetComponent<Slider>();
+        fill = gameObject.GetComponentInChildren<Image>();
     }
 
 
     public void SetMaxHealth(int health) {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void SetHealth(int health) {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
