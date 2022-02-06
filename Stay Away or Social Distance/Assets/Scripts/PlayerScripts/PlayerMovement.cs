846d727ee94a537541656768c06eca5987c0469c
S V <sv@Ss-MacBook-Pro.local>
2022-02-06 13:00:45 -0500

    fix shooting

--- Stay Away or Social Distance/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Stay Away or Social Distance/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -39,16 +39,4 @@ public class PlayerMovement : MonoBehaviour
 
         movement = new Vector2(moveX, moveY).normalized;
     }
-
-    void FaceMouse() {
-
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        if (mousePosition.x > 0f) {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        } else if (mousePosition.x < 0f) {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-        }
-    }
 }
