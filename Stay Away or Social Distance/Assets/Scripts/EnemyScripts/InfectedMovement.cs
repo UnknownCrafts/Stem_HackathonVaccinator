c2c2a3726f52f44ab4a68b54f253e3a731727d76
gaby <gabydinnerdavid@gmail.com>
2022-02-06 15:44:38 -0500

    fix infected targeting

--- Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedMovement.cs
+++ Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedMovement.cs
@@ -6,8 +6,8 @@ public class InfectedMovement : MonoBehaviour
 {
 
     public BoxCollider2D bounds = new BoxCollider2D();
-
-    private Transform target;
+    private List<int> targetList = new List<int>();
+    private Hashtable targetPositions = new Hashtable();
     private Vector2 choosePoint;
     public float moveSpeed;
     private Vector2 position;
@@ -24,19 +24,19 @@ public class InfectedMovement : MonoBehaviour
     }
     void SelectLocation()
     {
-		Vector2 boundsPos = new Vector2(bounds.transform.position.x, bounds.transform.position.y) + bounds.offset;
-		float randomPosX = Random.Range(boundsPos.x - bounds.size.x / 2, boundsPos.x + bounds.size.x / 2);
+        Vector2 boundsPos = new Vector2(bounds.transform.position.x, bounds.transform.position.y) + bounds.offset;
+        float randomPosX = Random.Range(boundsPos.x - bounds.size.x / 2, boundsPos.x + bounds.size.x / 2);
         float randomPosY = Random.Range(boundsPos.y - bounds.size.y / 2, boundsPos.y + bounds.size.y / 2);
-		choosePoint =new Vector2(randomPosX, randomPosY);
+        choosePoint = new Vector2(randomPosX, randomPosY);
     }
     private void FixedUpdate()
     {
 
         float step = moveSpeed * Time.deltaTime;
         canAttack += Time.deltaTime;
-        if (target != null)
+        if (targetList.Count != 0)
         {
-            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+            transform.position = Vector2.MoveTowards(transform.position, ((Transform)targetPositions[targetList[0]]).position, step);
         }
         else
         {
@@ -89,19 +89,24 @@ public class InfectedMovement : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            target = other.transform;
+            targetList.Add(other.GetInstanceID());
+            targetPositions.Add(other.GetInstanceID(), other.transform);
         }
         if (other.gameObject.tag == "Friendly")
         {
-            target = other.transform;
+
+            targetList.Add(other.GetInstanceID());
+            targetPositions.Add(other.GetInstanceID(), other.transform);
         }
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player" && target != null || other.gameObject.tag == "Friendly" && target != null)
+        if (other.gameObject.tag == "Player" && targetList.Count != 0 || other.gameObject.tag == "Friendly" && targetList.Count != 0)
         {
-            target = null;
+            targetList.Remove(other.GetInstanceID());
+            targetPositions.Remove(other.GetInstanceID());
+            print(targetList.Count);
         }
     }
 }
