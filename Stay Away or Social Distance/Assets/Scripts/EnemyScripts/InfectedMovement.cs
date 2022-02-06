7adefa053b03126e00650fd5935ba5cfbf6d0247
S V <sv@Ss-MacBook-Pro.local>
2022-02-06 09:04:18 -0500

    Enemy Attack and Patrol system

--- Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedMovement.cs
+++ Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedMovement.cs
@@ -8,28 +8,65 @@ public class InfectedMovement : MonoBehaviour
     public Vector2 highBounds;
     public Vector2 lowBounds;
 
-    private Vector2 target;
+    private Transform target;
+    private Vector2 choosePoint;
     public float moveSpeed;
     Rigidbody2D rb;
     private Vector2 position;
-
-    void Start()
-    {
-        SelectLocation();
-    }
+    [SerializeField] private int attackDamage = 10;
+	[SerializeField] private int attackSpeed = 1;
+    private float canAttack;
 
 
-    void Update()
-    {
-        position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
-        if (Vector2.Distance(target, position) < 0.001f)
-        {
-            SelectLocation();
-        }
-        gameObject.transform.position = Vector2.MoveTowards(position, target, Time.deltaTime * moveSpeed);
-    }
     void SelectLocation()
     {
-        target = new Vector2(Random.Range(lowBounds.x, highBounds.x), Random.Range(highBounds.y, lowBounds.y)).normalized;
+        choosePoint = new Vector2(Random.Range(lowBounds.x, highBounds.x), Random.Range(highBounds.y, lowBounds.y));
     }
+    private void FixedUpdate() {
+		if (target != null) {
+			float step = moveSpeed * Time.deltaTime;
+			transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+		} else {
+            position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+            if (Vector2.Distance(choosePoint, position) < 0.001f)
+            {
+                SelectLocation();
+            }
+            gameObject.transform.position = Vector2.MoveTowards(position, choosePoint, Time.deltaTime * moveSpeed);
+        }
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			if (attackSpeed <= canAttack) {
+				other.gameObject.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+				canAttack = 0f;
+			} else {
+				canAttack += Time.deltaTime;
+			}
+		}
+	}
+
+	private void OnCollisionStay2D(Collision2D other) {
+		if (other.gameObject.tag == "Player") {
+			if (attackSpeed <= canAttack) {
+				other.gameObject.GetComponent<PlayerHealth>().TakeDamage(attackDamage);
+				canAttack = 0f;
+			} else {
+				canAttack += Time.deltaTime;
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			target = other.transform;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			target = null;
+		}
+	}
 }
