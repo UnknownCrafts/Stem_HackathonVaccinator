33f02b4b7a82e2eb2136a1464138ddb80d86a7c7
S V <sv@Ss-MacBook-Pro.local>
2022-02-05 23:54:08 -0500

    finished enemy health system

--- Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedHealth.cs
+++ Stay Away or Social Distance/Assets/Scripts/EnemyScripts/InfectedHealth.cs
@@ -7,20 +7,19 @@ public class InfectedHealth : MonoBehaviour
     public int enemyMaxHealth = 100;
     int enemyCurrentHealth;
 
-    InfectedHealthBar enemyHealthBar;
+    public InfectedHealthBar enemyHealthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-        enemyHealthBar = GameObject.Find("EnemyCanvas").GetComponentInChildren<InfectedHealthBar>();
         enemyCurrentHealth = enemyMaxHealth;
         enemyHealthBar.SetEnemyMaxHealth(enemyMaxHealth);
     }
 
     void Update() {
-
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            EnemyTakeDamage(20);
+        
+        if (enemyCurrentHealth <= 0) {
+            Destroy(gameObject);
         }
     }
 
