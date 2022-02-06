ecb02c1adaedad6c1abd2a6f540691c72393559e
gaby <gabydinnerdavid@gmail.com>
2022-02-06 14:08:25 -0500

    try to fix npc movement

--- Stay Away or Social Distance/Assets/Scripts/FriendlyScripts/FriendlyMovement.cs
+++ Stay Away or Social Distance/Assets/Scripts/FriendlyScripts/FriendlyMovement.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class FriendlyMovement : MonoBehaviour
 {
 
-    public Vector2 highBounds;
-    public Vector2 lowBounds;
+    public BoxCollider2D bounds = new BoxCollider2D();
     private Transform target;
     private Vector2 choosePoint;
     public float moveSpeed;
     private Vector2 position;
 
-
+    void Start()
+    {
+        SelectLocation();
+    }
+    public void AddBounds(BoxCollider2D newBounds)
+    {
+        bounds = newBounds;
+    }
     void SelectLocation()
     {
-        choosePoint = new Vector2(Random.Range(lowBounds.x, highBounds.x), Random.Range(highBounds.y, lowBounds.y));
+        Vector2 boundsPos = new Vector2(bounds.transform.position.x, bounds.transform.position.y) + bounds.offset;
+        float randomPosX = Random.Range(boundsPos.x - bounds.size.x / 2, boundsPos.x + bounds.size.x / 2);
+        float randomPosY = Random.Range(boundsPos.y - bounds.size.y / 2, boundsPos.y + bounds.size.y / 2);
+        choosePoint = new Vector2(randomPosX, randomPosY);
+        print(choosePoint);
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         position = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+        print(position);
         if (Vector2.Distance(choosePoint, position) < 0.001f)
         {
             SelectLocation();
